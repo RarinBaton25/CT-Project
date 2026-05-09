1cb0d494e9af2275cc04cae5c7909c46e4c20355
Daniel Frost Svenningsen <rarinbaton25@gmail.com>
2026-05-09 20:50:53 +0200

    changes

--- turtlebot3_obstacle_detection.py
+++ turtlebot3_obstacle_detection.py
@@ -57,6 +57,10 @@ CHANNEL_IGNORE_DISTANCE = 1.0
 MAX_LINEAR_SPEED = 0.21
 MAX_ANGULAR_SPEED = 1.7
 
+# together, these make the front hemisphere
+DEG_90  = np.multiply(0.5, np.pi)
+DEG_270 = np.multiply(1.5, np.pi)
+
 class Turtlebot3ObstacleDetection(Node):
 
     def __init__(self):
@@ -71,11 +75,11 @@ class Turtlebot3ObstacleDetection(Node):
         self.tele_twist.linear.x = 0.0
         self.tele_twist.angular.z = 0.0
 
+        self.closest_obstacle_in_path:LaserData = None
+
         self.led = LED(17)
         self.led.off()
  
-        self.debug_counter = 0
-
         qos = QoSProfile(depth=10)
 
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', qos)
@@ -109,9 +113,6 @@ class Turtlebot3ObstacleDetection(Node):
         return any(datapoint.get_distance() < STOP_DISTANCE for datapoint in self.scan_ranges if datapoint.get_angle() < np.pi*0.15 or datapoint.get_angle() > np.pi*(2-0.15))
 
     def set_angular_speed_vs_linear_speed(self, ratio:float, turn_direction=TURN_LEFT):
-        
-
-        print("Ratio =", ratio)
         if not 0. <= ratio <= 1.:
             print("Ratio is out of bounds:", ratio)
             ratio = 1.
@@ -130,44 +131,53 @@ class Turtlebot3ObstacleDetection(Node):
         print("Setting angular.z =", self.tele_twist.angular.z)
 
     def distance_to_channel_wall(self, angle):
-        try:
-            return np.multiply(np.abs(np.divide(1, np.sin(angle))), WALL_DISTANCE)
-        except:
-            return 100
-
-    def obstacle_ahead(self):
-        # together, these make the fron hemisphere
-        deg_90  = np.multiply(0.5, np.pi)
-        deg_270 = np.multiply(1.5, np.pi)
+        if angle == 0.:
+            return 100 # something big
+        return np.multiply(np.abs(np.divide(1, np.sin(angle))), WALL_DISTANCE)
+        
+    def find_obstacle_ahead(self):
+        """
+        Finds closest obstacle in the path of the robot. Stores the closest obstacle in self.closest_obstacle_in_path, else None
+        """
         # print( [[data.get_angle(), f"{data.get_distance()} < {self.distance_to_channel_wall(data.get_angle())} < 3.5"] for data in self.scan_ranges if data.get_distance() < self.distance_to_channel_wall(data.get_angle()) < 3.5] )
+        self.closest_obstacle_in_path = None
         for data in self.scan_ranges:
-            if data.get_angle() < deg_90 or data.get_angle() > deg_270:
-                if data.get_distance() < self.distance_to_channel_wall(data.get_angle()) < CHANNEL_IGNORE_DISTANCE:
-                    return True
-        return False
+            if data.get_angle() < DEG_90 or data.get_angle() > DEG_270: #limit search to obstacles in front
+                if data.get_distance() < self.distance_to_channel_wall(data.get_angle()) < CHANNEL_IGNORE_DISTANCE: # is there an obstacle in our path?
+                    if self.closest_obstacle_in_path == None or data.get_distance() < self.closest_obstacle_in_path.get_distance():
+                        # update newest closest obstacle
+                        self.closest_obstacle_in_path = data
+
+    def avoid_obstacle(self):
+        turn_ratio = (self.closest_obstacle_in_path.get_distance() - STOP_DISTANCE)/(CHANNEL_IGNORE_DISTANCE - STOP_DISTANCE)
+        print("Turn ratio:", turn_ratio)
+        if self.closest_obstacle_in_path.get_angle() < DEG_90:
+            # obstacle is to the left
+            self.set_angular_speed_vs_linear_speed(turn_ratio, TURN_RIGHT)
+        elif self.closest_obstacle_in_path.get_angle() > DEG_270:
+            # obstacle is to the right
+            self.set_angular_speed_vs_linear_speed(turn_ratio, TURN_LEFT)
 
     def update(self):
         if self.has_scan_received:
-            
-            if self.obstacle_ahead():
-                # print("Found something!")
+            print("----")
+            self.find_obstacle_ahead()
+            if self.should_stop():
+                print("Stopping.")
+                self.led.off()
+                self.set_angular_speed_vs_linear_speed(1., TURN_LEFT)
+            elif self.closest_obstacle_in_path != None:
+                # deviate slightly
+                print("Deviating slightly.")
+                self.avoid_obstacle()
                 self.led.on()
             else:
-                # print("Nothing ahead.")
+                # continue forward
                 self.led.off()
-            # if self.should_stop():
-            #     self.led.on()
-            #     print("Stopping.")
-            #     self.set_angular_speed_vs_linear_speed(1., TURN_LEFT)
-            # elif self.obstacle_ahead():
-            #     pass
-            # else:
-            #     self.led.off()
-            #     print("Continuing.")
-            #     self.set_angular_speed_vs_linear_speed(0.)
-                
-            # # print("Publishing self.tele_twist.")
-            # self.cmd_vel_pub.publish(self.tele_twist)
+                print("Continuing.")
+                self.set_angular_speed_vs_linear_speed(0.)
+            
+            self.cmd_vel_pub.publish(self.tele_twist)
 
 
 def main(args=None):
